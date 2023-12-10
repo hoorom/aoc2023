a09cd0f9c374b6527dfcf9390f12c627a77c6e5a
RHoog <romain.hoog@kshuttle.io>
2023-12-10 21:13:24 +0100

    Initial commit

---
+++ src/CamelHand.java
@@ -0,0 +1,176 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class CamelHand implements Comparable<CamelHand> {
+
+    private String cards;
+
+    private Integer money;
+
+    public CamelHand(String cards, Integer money) {
+        this.cards = cards;
+        this.money = money;
+    }
+
+    public String getCards() {
+        return cards;
+    }
+
+    public void setCards(String cards) {
+        this.cards = cards;
+    }
+
+    public Integer getMoney() {
+        return money;
+    }
+
+    public void setMoney(Integer money) {
+        this.money = money;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof CamelHand camelHand)) {
+            return false;
+        }
+        return Objects.equals(cards, camelHand.cards);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cards);
+    }
+
+    @Override
+    public int compareTo(CamelHand o) {
+        if(!getKind().equals(o.getKind())) {
+            return getKind() - o.getKind();
+        }
+
+        for (int i = 0; i < 5; i++) {
+            char card = cards.charAt(i);
+            char otherCard = o.cards.charAt(i);
+            int cardValue = cardValue(card);
+            int otherCardValue = cardValue(otherCard);
+
+            if(cardValue != otherCardValue) {
+                return cardValue - otherCardValue;
+            }
+        }
+        return 0;
+    }
+
+    public static int cardValue(char c) {
+        List<Character> values = Arrays.asList('A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J');
+        return values.indexOf(c);
+    }
+
+    public int getNbJ() {
+        char[] charArray = cards.toCharArray();
+        int nbj = 0;
+        for (char c : charArray) {
+            if(c == 'J') {
+                ++nbj;
+            }
+        }
+        return nbj;
+    }
+
+    public Integer getKind() {
+        char[] sortedArray = cards.toCharArray();
+        Map<Character, Integer> occurences = new HashMap<>();
+        int nbJ = 0;
+        for (char c : sortedArray) {
+            if(c == 'J') {
+                ++nbJ;
+                continue;
+            }
+            occurences.putIfAbsent(c, 0);
+            occurences.put(c, occurences.get(c) + 1);
+        }
+
+
+
+        Collection<Integer> values = occurences.values();
+        if(values.contains(5)) {
+            return 0;
+        } else if (values.contains(4)) {
+            if (nbJ == 1) {
+                return 0;
+            }
+            return 1;
+        } else if (values.contains(3) && values.contains(2)) {
+            return 2;
+        } else if (values.contains(3)) {
+            if(nbJ == 1) {
+                return 1;
+            }
+
+            if(nbJ == 2) {
+                return 0;
+            }
+            return 3;
+        } else if (values.contains(2)) {
+
+            int nbPair = 0;
+            for (Integer value : values) {
+                if(value == 2) {
+                    ++nbPair;
+                }
+            }
+            if(nbPair == 2) {
+                if(nbJ == 1) {
+                    return 2;
+                }
+                return 4;
+            } else {
+
+                if (nbJ == 1) {
+                    return 3;
+                }
+
+                if (nbJ == 2) {
+                    return 1;
+                }
+
+                if (nbJ == 3) {
+                    return 0;
+                }
+                return 5;
+            }
+        }
+
+        if(nbJ == 1) {
+            return 5;
+        }
+
+        if(nbJ == 2) {
+            return 3;
+        }
+
+        if(nbJ == 3) {
+            return 1;
+        }
+
+        if(nbJ == 4) {
+            return 0;
+        }
+
+        if(nbJ == 5) {
+            return 0;
+        }
+        return 6;
+    }
+
+    @Override
+    public String toString() {
+        return "CamelHand{" + "cards='" + cards + '\'' + '}';
+    }
+}
---
+++ src/Day10.java
@@ -0,0 +1,144 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+import javax.swing.text.Position;
+
+public class Day10 {
+
+    public static List<List<Character>> map = new ArrayList<>();
+
+    public static AtomicInteger depth = new AtomicInteger(0);
+
+    public static String test0 = """
+            .....
+            .S-7.
+            .|.|.
+            .L-J.
+            .....""";
+
+    public static String test = """
+            7-F7-
+            .FJ|7
+            SJLL7
+            |F--J
+            LJ.LJ""";
+
+    public static void main(String[] args) {
+        String input = Day10Input.input;
+//        String input = test;
+//        String input = test0;
+        initMap(input);
+        System.out.println(getCharAtPos(new Position(2,2)));
+        Position initialPosition = findInitialPosition(input);
+        Position previousPosition = null;
+        Position transitionnal;
+        Character c = '0';
+        while (c != 'S') {
+            transitionnal = getNextPosition(initialPosition, previousPosition);
+            c = getCharAtPos(transitionnal);
+            previousPosition = initialPosition;
+            initialPosition = transitionnal;
+        }
+        System.out.println(depth);
+        System.out.println("Result : " +  (depth.get() >> 1));
+        //13539
+    }
+
+    private static Position getNextPosition(Position initialPosition, Position previousPosition) {
+        depth.incrementAndGet();
+        Character charAtCurrPos = getCharAtPos(initialPosition);
+        System.out.println(charAtCurrPos+" : " + initialPosition + " / " + depth);
+        Position nextPosition = computeNextPosition(initialPosition, previousPosition);
+        return nextPosition;
+    }
+
+    public static void initMap(String input) {
+        Stream<String> lines = input.lines();
+        List<String> inputLines = lines.toList();
+        for (String inputLine : inputLines) {
+            char[] charArray = inputLine.toCharArray();
+            ArrayList<Character> terrain = new ArrayList<>();
+            map.add(terrain);
+            for (char c : charArray) {
+                terrain.add(c);
+            }
+        }
+    }
+
+    public static Position findInitialPosition(String input) {
+        int pos = input.indexOf("S");
+        int size = map.getFirst().size();
+        int line = pos / size;
+        int column = pos % (size + 1);
+        Position position = new Position(line, column);
+        Character charAtPos = getCharAtPos(position);
+        assert charAtPos == 'S';
+        return position;
+    }
+
+    public static Position computeNextPosition(Position currentPosition, Position previousPosition) {
+        Character charAtPos = getCharAtPos(currentPosition);
+        Tuyau tuyau = Tuyau.fromChar(charAtPos);
+        assert tuyau != null;
+        List<Position> possiblePositions = Tuyau.getPossiblePositions(tuyau);
+
+        assert possiblePositions != null;
+        List<Position> modifiableList = new ArrayList<>(possiblePositions);
+        if (previousPosition != null && !modifiableList.isEmpty()) {
+            modifiableList.remove(previousPosition.diff(currentPosition));
+        }
+
+        assert modifiableList.size() == 1;
+
+        Position nextPosition = currentPosition.merge(modifiableList.getFirst());
+
+        //For test
+        Character nextChar = getCharAtPos(nextPosition);
+        if (nextChar != 'S') {
+            isMoveAllowed(modifiableList.getFirst(), nextChar);
+        }
+        ///////
+        return nextPosition;
+    }
+
+    public static boolean isMoveAllowed(Position relativePosition, Character c) {
+        if(relativePosition.column == 1) {
+            if(Arrays.asList('7', '-', 'J').contains(c)) {
+                return true;
+            }
+        } else if (relativePosition.column == -1) {
+            if(Arrays.asList('F', '-', 'L').contains(c)) {
+                return true;
+            }
+        } else if (relativePosition.line ==  1){
+            if(Arrays.asList('|', 'L', 'J').contains(c)) {
+                return true;
+            }
+        } else if (relativePosition.line == -1) {
+            if(Arrays.asList('|', '7', 'F').contains(c)) {
+                return true;
+            }
+        }
+        throw new RuntimeException("marche pas");
+    }
+
+    public static Character getCharAtPos(Position p) {
+        return map.get(p.line).get(p.column);
+    }
+
+
+    public record Position(int line, int column){
+
+        public Position merge(Position relativePosition) {
+            return new Position((line + relativePosition.line), column + relativePosition.column);
+        }
+
+        public Position diff(Position relativePosition) {
+            return new Position((line - relativePosition.line), column - relativePosition.column);
+        }
+
+    }
+
+}
